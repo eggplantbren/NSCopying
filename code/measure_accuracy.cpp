53243b92ac353cef1edaa5288f81abc5412595df
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-24 16:12:08 +0100

    Reproducing results from DNest paper

--- code/measure_accuracy.cpp
+++ code/measure_accuracy.cpp
@@ -17,16 +17,18 @@ int main(int argc, char** argv)
 	RNG rng;
 	rng.set_seed((argc == 1)?(time(0)):(atoi(argv[1])));
 
+	int num_particles = 50;
+
 	fstream fout("output.txt", ios::out);
 	while(true)
 	{
 		// Create a sampler
-		Sampler<SpikeSlab> sampler(100, 1000);
+		Sampler<SpikeSlab> sampler(num_particles, 100000/num_particles);
 		sampler.clear_output_files();
 		sampler.set_rng(rng);
 
 		// Do some iterations
-		sampler.run(100*100);
+		sampler.run(num_particles*100);
 
 		// Output the result (estimated logZ)
 		fout<<sampler.get_logZ()<<endl;
